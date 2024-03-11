b64afe3bacbdf36456adaf5a3769a13471f2ab02
Tanya Kataeva <tanya@kataev.pro>
2024-03-11 13:27:41 +0300

    Fix buttonPressed function

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else { return }
-        
-        print(buttonText)
-        
+        if let buttonText = sender.titleLabel?.text {
+            print(buttonText)
+        } else {
+            print("Button has no title.")
+        }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Hello!")
     }
 
 
